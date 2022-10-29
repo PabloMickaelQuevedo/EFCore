03c85a8d1ae4cb1b30b54060c61ae32d2a5e8168
PabloMickaelQuevedo <drag-rox@hotmail.com>
2022-10-29 18:22:17 -0300

    Criado as Classes Pedido.cs e PedidoItem.cs

---
+++ EFcore/Domain/Pedido.cs
@@ -0,0 +1,19 @@
+﻿using EFcore.ValueObjects;
+using System;
+using System.Collections.Generic;
+
+namespace EFcore.Domain
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public Cliente Cliente { get; set; }
+        public DateTime IniciadoEm { get; set; }
+        public DateTime FinalizadoEm { get; set; }
+        public TipoFrete TipoFrete { get; set; }
+        public StatusPedido StatusPedido { get; set; }
+        public string Obersavacao { get; set; }
+        public ICollection<PedidoItem> Itens { get; set; }
+    }
+}
