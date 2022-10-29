deb7853741667cc7614a5afb5315ecce03e855b1
PabloMickaelQuevedo <drag-rox@hotmail.com>
2022-10-29 18:36:01 -0300

    Adicionado a pasta Data e a classe ApllicationContext.cs

---
+++ EFcore/Data/ApllicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFcore.Data
+{
+    public class ApllicationContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data source=(localdb)\\mssqllocaldb.Initial Catalog=EFcore:Integrated Security=true");
+        }
+    }
+}
